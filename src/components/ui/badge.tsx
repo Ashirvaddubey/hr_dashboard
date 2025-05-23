b917510931c7118859fdab9f6c7c1b49905190bc
Ashirvad dubey <114186314+Ashirvaddubey@users.noreply.github.com>
2025-05-23 21:56:09 +0530

    Update badge.tsx

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,8 +1,6 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
-
 import { cn } from "@/lib/utils"
-
 const badgeVariants = cva(
   "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
@@ -22,7 +20,6 @@ const badgeVariants = cva(
     },
   }
 )
-
 export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
@@ -32,5 +29,4 @@ function Badge({ className, variant, ...props }: BadgeProps) {
     <div className={cn(badgeVariants({ variant }), className)} {...props} />
   )
 }
-
 export { Badge, badgeVariants }
